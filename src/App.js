e20670b6b1ca3e7d4c92486311d502351363c4b4
Giovanni Bedoya <giovedoya@gmail.com>
2023-03-09 20:41:17 +0100

    'Done'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Home from './views/Home';
+import Contact from './views/Contact';
+import Navbar from './components/Navbar';
+import Projects from './views/Projects';
+import ProjectsGames from './components/ProjectsGames';
+import ProjectsApps from './components/ProjectsApps';
+import ErrorPage from './views/ErrorPage';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="projects" element={<Projects />}>
+          <Route path="games" element={<ProjectsGames />} />
+          <Route path="apps" element={<ProjectsApps />} />
+        </Route>
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
     </div>
   );
 }
--- src/views/Projects.jsx
+++ src/views/Projects.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { NavLink, Outlet } from 'react-router-dom';
 
 export default function Projects() {
   return (
     <div>
       <h1>My projects</h1>
       <p>I have separated my projects into two categories: games I have designed and developed, and applications I have programmed.</p>
+      <nav>
+        <ul>
+          <li><NavLink to="/projects/games">See games</NavLink></li>
+          <li><NavLink to="/projects/apps">See apps</NavLink></li>
+        </ul>
+      </nav>
+      <Outlet />
     </div>
   )
 }
